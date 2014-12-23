6eac365fc0f79498790be71d4c2968aad0226d44
ChadCarey <chadcarey7@gmail.com>
2014-12-23 15:12:25 -0700

    added user_sport_routes model and tests are passing

--- spec/factories/user_sport_routes.rb
+++ spec/factories/user_sport_routes.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user_sport_route, :class => 'UserSportRoutes' do
-    user nil
-sport_route ""
-complete 1
+    user_id {FactoryGirl.create(:user).id}
+    sport_route_id {FactoryGirl.create(:sport_route).id}
+    complete 1
   end
 
 end
--- spec/models/user_sport_routes_spec.rb
+++ spec/models/user_sport_routes_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe UserSportRoutes, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    subject = FactoryGirl.create(:user_sport_route)
+    expect(subject).to be_valid
+  end
+  it "is invalid without a user" do
+    subject = FactoryGirl.build(:user_sport_route, user_id:nil)
+    expect(subject).to_not be_valid
+  end
+  it "is invalid without a sport_route" do
+    subject = FactoryGirl.build(:user_sport_route, sport_route_id:nil)
+    expect(subject).to_not be_valid
+  end
+  it "is invalid without a 'complete' value" do
+    subject = FactoryGirl.build(:user_sport_route, complete:nil)
+    expect(subject).to_not be_valid
+  end
+  it "is invalid a 'complete' value grater than 1" do
+    subject = FactoryGirl.build(:user_sport_route, complete:2)
+    expect(subject).to_not be_valid
+  end
+  it "is invalid a 'complete' value less than 0" do
+    subject = FactoryGirl.build(:user_sport_route, complete:-1)
+    expect(subject).to_not be_valid
+  end
 end
