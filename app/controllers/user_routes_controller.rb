409c836a7be03fabca18304d1453a7fea15519d9
ChadCarey <chadcarey7@gmail.com>
2014-12-24 09:51:15 -0700

    added user routes controller, not working yet

---
+++ app/controllers/user_routes_controller.rb
@@ -0,0 +1,32 @@
+class UserRoutesController < ApplicationController
+  
+  # gets
+    def showAll
+    end
+    
+    def showClimbed
+    end
+
+    def showUnclimbed
+    end
+
+  # posts
+    def addClimb
+    end  
+
+  # puts
+    def updateClimb
+    end
+
+  # deletes
+    def removeClimb
+    end
+
+private
+  def getAll
+  end
+  def getClimbed
+  end
+  def getUnclimbed
+  end
+end
